f83480196a7a87b54d23a5e7b1c72efcdefce85f
Revanth1902 <revanth19a@gmail.com>
2025-09-17 22:13:56 +0530

    Add requirements.txt and webhook.py

--- webhook.py
+++ webhook.py
@@ -81,5 +81,5 @@ def webhook():
 
 
 
-if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
